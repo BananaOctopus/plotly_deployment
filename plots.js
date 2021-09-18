6b7035c22ae166091629392e27472317ecec8210
BananaOctopus <84158312+BananaOctopus@users.noreply.github.com>
2021-09-18 17:19:27 -0400

    Lets see if this works

--- plots.js
+++ plots.js
@@ -1,37 +1,106 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+//Build function to read json file using d3
+function buildMetaData(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    console.log(metadata);
+
+  // Filter the data
+  var buildingArray = metadata.filter(sampleObj => sampleObj.id == sample);
+  var result = buildingArray[0];
+  // Use d3 to select the required panel
+  var panelData = d3.select("#sample-metadata");
+
+  // Clear the existing data in the html
+  panelData.html("");
+
+  // Use `Object.entries` to add each key and value pair to the panelData
+  Object.entries(result).forEach(([key, value]) => {
+    panelData.append("h6").text(`${key.toUpperCase()}: ${value}`);
+  });
 });
+}
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+    var sampleData = data.samples;
+    var buildingArray = sampleData.filter(sampleObj => sampleObj.id == sample);
+    var result = buildingArray[0];
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
+    // Build a Bubble Chart
+  var bubbleChart = {
+      title: "Bacteria Cultures Per Sample",
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+    };
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+
+    Plotly.newPlot("bubble", bubbleData, bubbleChart);
+    
+    //Create a horizontal bar chart
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    var barData = [
+      {
+        y: yticks,
+        x: sample_values.slice(0, 10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+      }
+    ];
+
+    var chartLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+    Plotly.newPlot("bar", barData, chartLayout);
+  });
 };
 
-// data
-var data = [trace1];
+function init() {
+  // Grab a reference to the dropdown select element
+  var selectDropdown = d3.select("#selDataset");
 
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
+  // Populate the select options by using the list of sample names
+  d3.json("samples.json").then((data) => {
+    var name = data.names;
+
+    name.forEach((sample) => {
+      selectDropdown
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    })
+
+    // Use the sample data from the list to build the plots
+    var sampleData = name[0];
+    buildCharts(sampleData);
+    buildMetaData(sampleData);
+  });
 };
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetaData(newSample);
+};
+
+
+// Initialize the dashboard
+init()
