b91ddcc93498991ed0926fc4bebb60015c8be8c9
BananaOctopus <84158312+BananaOctopus@users.noreply.github.com>
2021-09-19 20:08:20 -0400

    Possibly final commit

--- bubble_chart.js
+++ bubble_chart.js
@@ -9,16 +9,21 @@ function buildCharts(sample) {
     Plotly.newPlot(); 
 
     // 1. Create the trace for the bubble chart.
-    var bubbleData = [
-   
-    ];
-
+    var bubbleData = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: { color: otu_ids, size: sample_values, colorscale: "Earth" },
+    };
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
-      
+      title: "Bacteria Cultures Per Sample",
+      hovermode: "closest",
+      xaxis: { title: "OTU IDs" },
     };
 
     // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot(); 
+    Plotly.newPlot("plotAreaBubble", [bubbleData], bubbleLayout); 
   });
 }
